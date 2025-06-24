6e669a0bf9027bd99995fa456439eff8c2faaf2b
Ali Madooei <alimadooei@gmail.com>
2025-06-23 20:58:58 -0400

    refactor: simplify App component by removing unnecessary wrapper div

--- examples/with-tailwind4/src/App.tsx
+++ examples/with-tailwind4/src/App.tsx
@@ -1,11 +1,7 @@
 import CardsDemoPage from "./pages/cards-demo-page";
 
 function App() {
-  return (
-    <div className="min-h-screen max-w-2xl mx-auto">
-      <CardsDemoPage />
-    </div>
-  );
+  return <CardsDemoPage />;
 }
 
 export default App;
