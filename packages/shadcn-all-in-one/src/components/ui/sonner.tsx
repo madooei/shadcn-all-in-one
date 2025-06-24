8ab1c21d3d307c6aacd184aa067ae26582c7dc83
Ali Madooei <alimadooei@gmail.com>
2025-06-24 11:25:06 -0400

    refactor: enhance Toaster component to accept theme prop and remove useTheme dependency

--- packages/shadcn-all-in-one/src/components/ui/sonner.tsx
+++ packages/shadcn-all-in-one/src/components/ui/sonner.tsx
@@ -1,12 +1,13 @@
-import { useTheme } from "next-themes";
 import { Toaster as Sonner, type ToasterProps } from "sonner";
 
-const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme();
+export interface SonnerProps extends ToasterProps {
+  theme: ToasterProps["theme"];
+}
 
+const Toaster = ({ theme, ...props }: ToasterProps) => {
   return (
     <Sonner
-      theme={theme as ToasterProps["theme"]}
+      theme={theme}
       className="toaster group"
       style={
         {
