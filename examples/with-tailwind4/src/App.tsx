5f292958b1f4244c6a7bbcb557e7fd5377947a33
Ali Madooei <alimadooei@gmail.com>
2025-06-23 22:07:38 -0400

    refactor: update package-lock and package.json for dependency alignment and rename demo package

--- examples/with-tailwind4/src/App.tsx
+++ examples/with-tailwind4/src/App.tsx
@@ -1,7 +1,9 @@
+import { CardsCalendar } from "./components/calendar";
 import CardsDemoPage from "./pages/cards-demo-page";
 
 function App() {
-  return <CardsDemoPage />;
+  // return <CardsDemoPage />;
+  return (<CardsCalendar />);
 }
 
 export default App;
