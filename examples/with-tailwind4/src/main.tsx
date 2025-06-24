27c1037e501e4065bd46c5c9595e7c49b9d581f1
Ali Madooei <alimadooei@gmail.com>
2025-06-23 20:58:53 -0400

    refactor: remove unused global styles and update CSS imports for Tailwind 4 example

--- examples/with-tailwind4/src/main.tsx
+++ examples/with-tailwind4/src/main.tsx
@@ -1,6 +1,4 @@
-import "./index.css"
-import "./globals-v4.css";
-// import "@madooei/shadcn-all-in-one/globals-v4.css";
+import "./styles/index.css"
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
