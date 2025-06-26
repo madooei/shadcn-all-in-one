35d4316bad872de1f9cc7291b90c8401a7d01c77
Ali Madooei <alimadooei@gmail.com>
2025-06-25 22:28:21 -0400

    style: update Card component styling for better responsiveness

--- examples/with-tailwind4/src/components/calendar.tsx
+++ examples/with-tailwind4/src/components/calendar.tsx
@@ -7,7 +7,7 @@ const start = new Date(2023, 5, 5);
 
 export function CardsCalendar() {
   return (
-    <Card className="max-w-[260px] overflow-auto">
+    <Card className="mx-auto w-fit sm:w-full overflow-auto">
       <CardContent className="p-1">
         <Calendar
           numberOfMonths={1}
