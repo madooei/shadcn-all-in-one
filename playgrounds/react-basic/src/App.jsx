070e5392e7572b65f979234de95bc5af247ebd9f
Ali Madooei <alimadooei@gmail.com>
2025-07-03 00:38:13 -0400

    chore: set up React project with Vite, ESLint, Tailwind CSS, and initial components

---
+++ playgrounds/react-basic/src/App.jsx
@@ -0,0 +1,17 @@
+import { Button } from '@madooei/shadcn-all-in-one/button';
+import {
+  TableOptions,
+  TableOptionsResolved,
+  RowData,
+  createTable,
+} from '@tanstack/table-core'
+
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App
